250b704d805d6cf9410161d84ea3da705be0c87c
emiliemoulin <emiliemoulin021218@gmail.com>
2023-01-12 11:11:07 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿Village newVillage = new Village("village1");
+Console.WriteLine(newVillage.getName()); // "village1"
+class Village
+{
+    private string name;
+
+    public Village(string name)
+    {
+        this.name = name;
+    }
+
+    public string getName()
+    {
+        return name;
+    }
+}
+class Ressources
+{
+    private int woods;
+    private int stones;
+
+    public Ressources(int woods, int stones)
+    {
+        this.woods = woods;
+        this.stones = stones;
+    }
+
+    public int getWood()
+    {
+        return woods;
+    }
+
+    public int getStone()
+    {
+        return stones;
+    }
+}
+
+
+
+
